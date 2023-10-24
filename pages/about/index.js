db80ca3f007b886ae6c9528a5b6325c3c103d733
shivamsen23 <97625468+shivamsen23@users.noreply.github.com>
2023-10-24 20:15:34 +0530

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -33,8 +33,7 @@ import {
   import { fadeIn} from '../../variants';
   import Avatar from '../../components/Avatar';
 import Circles from '../../components/Circles';
-//import {resume} from '../../public/resume.pdf'
-//  data
+
 const aboutData = [
   {
     title: 'skills',
@@ -124,8 +123,7 @@ import CountUp from 'react-countup'
 const About = () => {
   const [index, setIndex] = useState(0);
   const handleDownloadResume = () => {
-    // Add logic to initiate the download of your resume
-    // For example, you can create an anchor element and click it programmatically
+    
     const anchor = document.createElement('a');
     anchor.href = '/resume.pdf';
     anchor.download = 'your_resume.pdf';
@@ -134,7 +132,7 @@ const About = () => {
   return (
     <div className='h-full bg-primary/30 py-52 text-center xl:text-left'>
       <Circles />
-      {/* avatar img */}
+     
       <motion.div
         variants={fadeIn('right' ,0.2)}
         initial='hidden'
@@ -142,7 +140,7 @@ const About = () => {
         exit='hidden'
         className='hidden sm:flex absolute bottom-[-5] -left-[187px]'
       >
-       <Avatar /> {/* Adjust width and height as needed */}
+       <Avatar />
       </motion.div>
       <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-19'>
       <div className='flex-1 flex flex-col justify-center'>
@@ -159,7 +157,7 @@ const About = () => {
 <p className='max-w-[500px] mx-auto xl:mx-0 mb-6 xl: mb-12 px-2 sm:px-5'>
 Experienced MERN stack developer skilled in JavaScript, React, HTML, and CSS. Proficient in building dynamic interfaces, scalable React apps, and efficient, sustainable code for engaging web applications.
 </p>
-{/* counters */}
+
 <div>
 <button
   onClick={handleDownloadResume}
@@ -171,137 +169,44 @@ Experienced MERN stack developer skilled in JavaScript, React, HTML, and CSS. Pr
 </div>
 </div>
         <div className='flex flex-col w-full xl:max-w-[48%] h-[180px] mt-5'>
-        <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
-{/* {aboutData.map ((item, itemIndex) => {
-return (
-<div
-key={itemIndex}
-className={`${
-index=== itemIndex &&
-'text-accent after:w-[100%] after:bg-accent after: transition-all after:duration-300'
-} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-onClick={() => setIndex(itemIndex)}
->
-{item.title}
-</div>
-
-);
-})} */}
-// {aboutData.map((item, itemIndex) => {
-//   return (
-//     <div
-//       key={itemIndex} // Add key here
-//       className={`${
-//         index === itemIndex &&
-//         'text-accent after:w-[100%] after:bg-accent after: transition-all after:duration-300'
-//       } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-//       onClick={() => setIndex(itemIndex)}
-//     >
-//       {item.title}
-//     </div>
-//   );
-// })}
-{aboutData.map((item, itemIndex) => {
-  return (
-    <div
-      key={itemIndex}
-      className={`${
-        index === itemIndex &&
-        'text-accent after:w-[100%] after:bg-accent after: transition-all after:duration-300'
-      } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
-      onClick={() => setIndex(itemIndex)}
-    >
-      {item.title}
+  <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+      {aboutData.map((item, itemIndex) => (
+        <div
+          key={itemIndex}
+          className={`${
+            index === itemIndex &&
+            'text-accent after:w-[100%] after:bg-accent after: transition-all after:duration-300'
+          } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+          onClick={() => setIndex(itemIndex)}
+        >
+          {item.title}
+        </div>
+      ))}
     </div>
   );
 })}
 </div>
 <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
-{/* {aboutData[index].info.map((item,itemIndex)=>{
-  return(
-    <div key={itemIndex}
-    className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
-    >
-      <div className='font-light mb-2 md:mb-0'>{item.title}</div>
-      <div className='hidden md:flex'>-</div>
-      <div>{item.stage}</div>
-      <div className='flex gap-x-4'>
-        {item.icons?.map((icon,itemIndex)=>{
-          return <div className='text-2xl text-white'>{icon}</div>
-        })}
+      {aboutData[index].info.map((item, itemIndex) => (
+        <div
+          key={itemIndex}
+          className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
+        >
+          <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+          <div className='hidden md:flex'>-</div>
+          <div>{item.stage}</div>
+          <div className='flex gap-x-4'>
+            {item.icons?.map((icon, iconIndex) => (
+              <div key={iconIndex} className='text-2xl text-white'>
+                {icon}
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-
-     
-  )
-})} */}
-
-// {aboutData[index].info.map((item, itemIndex) => {
-//   return (
-//     <div
-//       key={itemIndex} // Add key here
-//       className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
-//     >
-//       <div className="font-light mb-2 md:mb-0">{item.title}</div>
-//       <div className="hidden md:flex">-</div>
-//       <div>{item.stage}</div>
-//       <div className="flex gap-x-4">
-//         {item.icons?.map((icon, innerItemIndex) => {
-//           return (
-//             <div key={innerItemIndex} className="text-2xl text-white">
-//               {icon}
-//             </div>
-//           );
-//         })}
-//   {aboutData[index].info.map((item, itemIndex) => {
-//   return (
-//     <div
-//       key={itemIndex}
-//       className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
-//     >
-//       <div className='font-light mb-2 md:mb-0'>{item.title}</div>
-//       <div className='hidden md:flex'>-</div>
-//       <div>{item.stage}</div>
-//       <div className='flex gap-x-4'>
-//         {item.icons?.map((icon, iconIndex) => {
-//           return <div key={iconIndex} className='text-2xl text-white'>{icon}</div>;
-//         })}
-//       </div>
-//     </div>
-//   );
-// })}
-//       </div>
-//     </div>
-//   );
-// })}
-
-// </div>
-//         </div>
-
-
-//       </div>
-//     </div>
-//   );
-// };
-{aboutData[index].info.map((item, itemIndex) => {
-  return (
-    <div
-      key={itemIndex}
-      className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
-    >
-      <div className='font-light mb-2 md:mb-0'>{item.title}</div>
-      <div className='hidden md:flex'>-</div>
-      <div>{item.stage}</div>
-      <div className='flex gap-x-4'>
-        {item.icons?.map((icon, iconIndex) => {
-          return <div key={iconIndex} className='text-2xl text-white'>{icon}</div>;
-        })}
-      </div>
+      ))}
     </div>
-  );
-})}
-      </div>
-    </div>
+
+    // ... rest of your JSX code
   );
 };
 
