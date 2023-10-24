67ed1c831eacf8f5e1b4c0b8872cb28b66c56980
shivamsen23 <97625468+shivamsen23@users.noreply.github.com>
2023-10-24 19:59:21 +0530

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -187,10 +187,24 @@ onClick={() => setIndex(itemIndex)}
 
 );
 })} */}
+// {aboutData.map((item, itemIndex) => {
+//   return (
+//     <div
+//       key={itemIndex} // Add key here
+//       className={`${
+//         index === itemIndex &&
+//         'text-accent after:w-[100%] after:bg-accent after: transition-all after:duration-300'
+//       } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+//       onClick={() => setIndex(itemIndex)}
+//     >
+//       {item.title}
+//     </div>
+//   );
+// })}
 {aboutData.map((item, itemIndex) => {
   return (
     <div
-      key={itemIndex} // Add key here
+      key={itemIndex}
       className={`${
         index === itemIndex &&
         'text-accent after:w-[100%] after:bg-accent after: transition-all after:duration-300'
@@ -201,7 +215,6 @@ onClick={() => setIndex(itemIndex)}
     </div>
   );
 })}
-
 </div>
 <div className='py-2 xl:py-6 flex flex-col gap-y-2 xl:gap-y-4 items-center xl:items-start'>
 {/* {aboutData[index].info.map((item,itemIndex)=>{
@@ -223,23 +236,40 @@ onClick={() => setIndex(itemIndex)}
   )
 })} */}
 
-{aboutData[index].info.map((item, itemIndex) => {
+// {aboutData[index].info.map((item, itemIndex) => {
+//   return (
+//     <div
+//       key={itemIndex} // Add key here
+//       className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+//     >
+//       <div className="font-light mb-2 md:mb-0">{item.title}</div>
+//       <div className="hidden md:flex">-</div>
+//       <div>{item.stage}</div>
+//       <div className="flex gap-x-4">
+//         {item.icons?.map((icon, innerItemIndex) => {
+//           return (
+//             <div key={innerItemIndex} className="text-2xl text-white">
+//               {icon}
+//             </div>
+//           );
+//         })}
+  {aboutData[index].info.map((item, itemIndex) => {
   return (
     <div
-      key={itemIndex} // Add key here
-      className="flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60"
+      key={itemIndex}
+      className='flex-1 flex flex-col md:flex-row max-w-max gap-x-2 items-center text-white/60'
     >
-      <div className="font-light mb-2 md:mb-0">{item.title}</div>
-      <div className="hidden md:flex">-</div>
+      <div className='font-light mb-2 md:mb-0'>{item.title}</div>
+      <div className='hidden md:flex'>-</div>
       <div>{item.stage}</div>
-      <div className="flex gap-x-4">
-        {item.icons?.map((icon, innerItemIndex) => {
-          return (
-            <div key={innerItemIndex} className="text-2xl text-white">
-              {icon}
-            </div>
-          );
+      <div className='flex gap-x-4'>
+        {item.icons?.map((icon, iconIndex) => {
+          return <div key={iconIndex} className='text-2xl text-white'>{icon}</div>;
         })}
+      </div>
+    </div>
+  );
+})}
       </div>
     </div>
   );
